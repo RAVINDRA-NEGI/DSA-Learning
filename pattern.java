a14b74457dcb8c658563d16c9bdf32251551661e
RAVINDRA-NEGI <153095958+RAVINDRA-NEGI@users.noreply.github.com>
2024-11-13 10:58:25 +0530

    added some techniques

---
+++ pattern.java
@@ -0,0 +1,11 @@
+public class pattern {
+    public static void main(String[] args) {
+     System.out.println(count(4569));
+      
+}
+
+public static int count ( int n){
+    int count = (int)(log10(n) + 1)
+    return count;
+}
+}
