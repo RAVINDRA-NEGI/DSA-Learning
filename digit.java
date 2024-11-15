7566a472babe2f87b1a3a07e671774162bb9c268
RAVINDRA-NEGI <153095958+RAVINDRA-NEGI@users.noreply.github.com>
2024-11-15 13:13:53 +0530

    added some tricks

--- digit.java
+++ digit.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class digit {
     public static void main(String[] args) {
-        System.out.println(armstrongNumber(371));
+        System.out.println(alldivisors(4));
     }
 
     public static boolean armstrongNumber(int n){
@@ -19,4 +22,44 @@ public class digit {
             return false;
         }
     }
+
+        public static List<Integer>  divisors(int d){
+          List<Integer> count  = new ArrayList<>();
+            for( int i = 2; i <= d; i++){
+                if (d%i == 0){
+                    count.add(i);
+                }
+                
+            }
+            count.sort(Integer::compareTo);
+            
+            return count;
+        }
+
+        public static int sumOfAllDivisors(int n){
+           int sum = 0;
+            for (int i = 1; i <=n; i++){
+                for(int j = 1; j<= i; j++){
+                    if(i%j == 0){
+                        sum += j;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        // optimize version of sum of all divisors
+        public static  long  alldivisors(int n){
+            long sum = 0;
+
+     
+            for (int i = 1; i <= n; i++) {
+                
+                for (int j = i; j <= n; j += i) {
+                    sum += i;
+                }
+            }
+    
+            return sum;
+        }
 }
